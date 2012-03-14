b8e8bcd59c32f852934b91ba08d6c82772448cb1
etorreborre <etorreborre@yahoo.com>
2012-03-14 22:54:06 +1100

    refactored the MultiwayTreesSpec

--- src/test/scala/s99/MultiwayTreesSpec.scala
+++ src/test/scala/s99/MultiwayTreesSpec.scala
@@ -1,71 +1,77 @@
 package s99
 
-class MultiwayTreesSpec extends MultiwayTreesProblems { def is =
+import org.specs2.mutable.Specification
+
+class MultiwayTreesSpec extends Specification with MultiwayTreesSolutions {
   """
-   A multiway tree is composed of a root element and a (possibly empty) set of successors which are multiway trees
-   themselves. A multiway tree is never empty. The set of successor trees is sometimes called a forest.
+  A multiway tree is composed of a root element and a (possibly empty) set of successors which are multiway trees
+  themselves. A multiway tree is never empty. The set of successor trees is sometimes called a forest.
 
-   The code to represent these is somewhat simpler than the code for binary trees, partly because we don't separate
-   classes for nodes and terminators, and partly because we don't need the restriction that the value type be ordered.
+  The code to represent these is somewhat simpler than the code for binary trees, partly because we don't separate
+  classes for nodes and terminators, and partly because we don't need the restriction that the value type be ordered.
 
-         case class MTree[+T](value: T, children: List[MTree[T]]) {
-           def this(value: T) = this(value, List())
-           override def toString = "M(" + value.toString + " {" + children.map(_.toString).mkString(",") + "})"
-         }
+        case class MTree[+T](value: T, children: List[MTree[T]]) {
+          def this(value: T) = this(value, List())
+          override def toString = "M(" + value.toString + " {" + children.map(_.toString).mkString(",") + "})"
+        }
 
-         object MTree {
-           def apply[T](value: T) = new MTree(value, List())
-           def apply[T](value: T, children: List[MTree[T]]) = new MTree(value, children)
-         }
+        object MTree {
+          def apply[T](value: T) = new MTree(value, List())
+          def apply[T](value: T, children: List[MTree[T]]) = new MTree(value, children)
+        }
 
-   The example tree is, thus:
+  The example tree is, thus:
 
-        MTree('a', List(MTree('f', List(MTree('g'))), MTree('c'), MTree('b', List(MTree('d'), MTree('e')))))
-  """                                                                                                                   ^
+       MTree('a', List(MTree('f', List(MTree('g'))), MTree('c'), MTree('b', List(MTree('d'), MTree('e')))))
   """
-   *Count the nodes of a multiway tree*
 
-   Write a method nodeCount which counts the nodes of a given multiway tree"""                                          ! P70C^
-  """
-   *Tree construction from a node string*
+  """ Count the nodes of a multiway tree
+  Write a method nodeCount which counts the nodes of a given multiway tree""" >>
+  { MTree('a', List(MTree('f'))).nodeCount === 2 }
 
-   We suppose that the nodes of a multiway tree contain single characters. In the depth-first order sequence of its
-   nodes, a special character ^ has been inserted whenever, during the tree traversal, the move is a backtrack to the
-   previous level. By this rule, the tree in the figure opposite is represented as:
+  """ Tree construction from a node string
 
-         afg^^c^bd^e^^^
+  We suppose that the nodes of a multiway tree contain single characters. In the depth-first order sequence of its
+  nodes, a special character ^ has been inserted whenever, during the tree traversal, the move is a backtrack to the
+  previous level. By this rule, the tree in the figure opposite is represented as:
 
-   Define the syntax of the string and write a function string2MTree to construct an MTree from a String. Make the
-   function an implicit conversion from String. Write the reverse function, and make it the `show` method of MTree"""   ! P70^
-  """
-   *Determine the internal path length of a tree*
+       afg^^c^bd^e^^^
 
-   We define the internal path length of a multiway tree as the total sum of the path lengths from the root to all
-   nodes of the tree. By this definition, the tree in the figure of problem P70 has an internal path length of 9.
-   Write a method internalPathLength to return that sum"""                                                              ! P71^
-  """
-   *Construct the postorder sequence of the tree nodes*
+  Define the syntax of the string and write a function string2MTree to construct an MTree from a String. Make the
+  function an implicit conversion from String. Write the reverse function, and make it the `show` method of MTree""" >>
+  { MTree('a', List(MTree('f', List(MTree('g'))), MTree('c'), MTree('b', List(MTree('d'), MTree('e'))))).show ===
+     "afg^^c^bd^e^^^" }
+
+  """ Determine the internal path length of a tree
+
+  We define the internal path length of a multiway tree as the total sum of the path lengths from the root to all
+  nodes of the tree. By this definition, the tree in the figure of problem P70 has an internal path length of 9.
+  Write a method internalPathLength to return that sum""" >>
+  { "afg^^c^bd^e^^^".internalPathLength === 9 }
+
+  """ Construct the postorder sequence of the tree nodes
 
    Write a method postorder which constructs the postorder sequence of the nodes of a multiway tree. The result should
-   be a List"""                                                                                                         ! P72^
-  """
-   *Lisp-like tree representation*
+   be a List""" >>
+   { "afg^^c^bd^e^^^".postorder === List('g', 'f', 'c', 'd', 'e', 'b', 'a') }
 
-   There is a particular notation for multiway trees in Lisp. Lisp is a prominent functional programming language.
-   In Lisp almost everything is a list. Our example tree would be represented in Lisp as (a (f g) c (b d e)). The
-   following pictures give some more examples.
+  """ Lisp-like tree representation
 
-   Note that in the "lispy" notation a node with successors (children) in the tree is always the first element in a
-   list, followed by its children. The "lispy" representation of a multiway tree is a sequence of atoms and parentheses
-   '(' and ')', with the atoms separated by spaces. We can represent this syntax as a Scala String. Write a method
-   lispyTree which constructs a "lispy string" from an MTree
+  There is a particular notation for multiway trees in Lisp. Lisp is a prominent functional programming language.
+  In Lisp almost everything is a list. Our example tree would be represented in Lisp as (a (f g) c (b d e)). The
+  following pictures give some more examples.
 
-   As a second, even more interesting, exercise try to write a method that takes a "lispy" string and turns it into a
-   multiway tree.
+  Note that in the "lispy" notation a node with successors (children) in the tree is always the first element in a
+  list, followed by its children. The "lispy" representation of a multiway tree is a sequence of atoms and parentheses
+  '(' and ')', with the atoms separated by spaces. We can represent this syntax as a Scala String. Write a method
+  lispyTree which constructs a "lispy string" from an MTree
 
-   [Note: Much of this problem is taken from the wording of the same problem in the Prolog set. This is certainly one
-    way of looking at Lisp notation, but it's not how the language actually represents that syntax internally. I can
-    elaborate more on this, if requested"""                                                                             ! P73^
-                                                                                                                        end
+  As a second, even more interesting, exercise try to write a method that takes a "lispy" string and turns it into a
+  multiway tree.
+
+  [Note: Much of this problem is taken from the wording of the same problem in the Prolog set. This is certainly one
+   way of looking at Lisp notation, but it's not how the language actually represents that syntax internally. I can
+   elaborate more on this, if requested""" >>
+  { MTree("a", List(MTree("b", List(MTree("c"))))).lispyTree === "(a (b c))" }
 
 }
