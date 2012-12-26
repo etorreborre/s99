8787bfe7319d3a433c2d58819eb1c37c56ea36ca
Hugo Ferreira <hugo@shiftforward.eu>
2012-12-26 17:46:19 +0000

    Bumped all dependencies to work with Scala 2.10.0. Fixed handling of ??? to be consistent with Odersky's ProgFun.

--- src/main/scala/s99/Solutions.scala
+++ src/main/scala/s99/Solutions.scala
@@ -1,7 +1,6 @@
 package s99
 
-import org.specs2.matcher.ThrownExpectations
-
-object Solutions extends ThrownExpectations {
-  lazy val ??? = skipped("todo")
+object Solutions {
+  def ??? : Nothing = throw new Error("an implementation is missing")
+  type ??? = Nothing
 }
