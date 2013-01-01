b81f3937360aed6c2be148a999422093d1969cac
Rui Gonçalves <ruippeixotog@gmail.com>
2013-01-01 19:05:02 +0000

    Reordered arithmetic function stubs to the same order as the problems.

--- src/main/scala/s99/ArithmeticSolutions.scala
+++ src/main/scala/s99/ArithmeticSolutions.scala
@@ -12,19 +12,21 @@ trait ArithmeticSolutions {
     def isPrime: Boolean = ???
     def isCoprimeTo(n: Int): Boolean = ???
     def totient: Int = ???
-    def improvedTotient: Int = ???
     def primeFactors: List[Int] = ???
     def primeFactorMultiplicity: List[(Int, Int)] = ???
     def primeFactorMultiplicityMap: Map[Int, Int] = ???
+    def improvedTotient: Int = ???
     def listPrimesinRange(r: Range): List[Int] = ???
     def goldbach: (Int, Int) = ???
 
   }
 
-  def primes: Stream[Int] = ???
   def gcd(m: Int, n: Int): Int = ???
   def listPrimesinRange(r: Range): List[Int] = ???
   def printGoldbachList(r: Range): List[String] = ???
   def printGoldbachListLimited(r: Range, limit: Int): List[String] = ???
+
+  // Optional but possibly useful exercise: not in original s-99 problems
+  def primes: Stream[Int] = ???
 
 }
